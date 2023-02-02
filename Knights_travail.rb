831e6c795ced0b1c38ffad5e8559b997205573b6
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-01 23:17:08 -0500

    create valid moves method

--- Knights_travail.rb
+++ Knights_travail.rb
@@ -11,6 +11,13 @@ class KnightPathFinder
         [ 2,  1]
       ]
 
+      def self.valid_moves(pos)
+        cur_x, cur_y = pos
+        MOVES.select do |dx, dy|
+          new_x, new_y = cur_x + dx, cur_y + dy
+          new_x.between?(0, 7) && new_y.between?(0, 7)
+        end
+      end
 
-      
+
 end
