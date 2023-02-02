c74f3d58571c766ca4afa9beaf61c4134e1861af
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-01 23:13:49 -0500

    set range of position

---
+++ Knights_travail.rb
@@ -0,0 +1,16 @@
+class KnightPathFinder
+    attr_reader :start_pos
+    VALID_POS = [
+        [-2, -1],
+        [-2,  1],
+        [-1, -2],
+        [-1,  2],
+        [ 1, -2],
+        [ 1,  2],
+        [ 2, -1],
+        [ 2,  1]
+      ]
+
+
+      
+end
